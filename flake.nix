c0d7e65594c1595133c2ae7b1d9e254603c62679
Danny Huynh <danny@nextsemi.com>
2025-05-19 18:50:28 -0400

    Fix flake configuration

--- flake.nix
+++ flake.nix
@@ -2,28 +2,17 @@
   description = "My NixOS configurations";
 
   inputs = {
-    # Stable NixOS 24.05 (matches your running system)
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-
-    # Home Manager (release-24.05 branch)
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Hardware-specific configurations
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-
-    # Utility functions
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, home-manager, nixos-hardware, flake-utils, ... }@inputs:
     let
-      # System architecture (x86_64 unless you have ARM devices)
       system = "x86_64-linux";
-
-      # Common modules shared across all machines
+      pkgs = import nixpkgs { inherit system; };
+      
       commonModules = [
         home-manager.nixosModules.home-manager
         {
@@ -31,34 +20,23 @@
           home-manager.useUserPackages = true;
           home-manager.users.dhuynh = import ./users/dhuynh.nix;
         }
-        ./modules/core.nix  # Your shared base configuration
       ];
     in {
       nixosConfigurations = {
-        # Primary machine (matches your actual hostname 'nixos')
         nixos = nixpkgs.lib.nixosSystem {
           inherit system;
           modules = commonModules ++ [
             ./hosts/nixos/default.nix
-            # Intel-specific optimizations (remove if not applicable)
             nixos-hardware.nixosModules.common-cpu-intel
-          ];
-        };
-
-        # Example server configuration
-        myserver = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = commonModules ++ [
-            ./hosts/myserver/default.nix
           ];
         };
       };
 
-      # Optional: Development shells
+      # Fixed devShells section
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
-          alejandra  # Nix formatter
-          statix     # Nix linter
+          alejandra
+          statix
         ];
       };
     };
